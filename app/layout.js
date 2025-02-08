ea631f475eb1f7fc7c248aa18186abdc6d5a20f9
r2hu1 <hi@rahul.eu.org>
2025-02-08 22:26:58 +0530

    lyout

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import Header from "@/components/page/header";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+export const metadata = {
+  title: "AiBase",
+  description: "AiBase helps you discover AI tools like chatbots, image generators, and more—all in one place with names, descriptions, links, and icons.",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>
+          <Header/>
+        {children}
+      </body>
+    </html>
+  );
+}
